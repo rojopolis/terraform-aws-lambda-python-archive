8e018cc619877d5673b3d3b10df88d4ed0dec1eb
Robert Jordan <rjordan@deba.cl>
2018-09-27 15:24:37 -0700

    Specify python3

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 data "external" "lambda_archive" {
-    program = ["python", "${path.module}/scripts/build_lambda.py"]
+    program = ["python3", "${path.module}/scripts/build_lambda.py"]
     query = {
         src_dir     = "${var.src_dir}"
         output_path = "${var.output_path}"
